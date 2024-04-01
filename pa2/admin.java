26943074e63b30f87f265119b907b766bb3b1ae0
Krish Jetly <Kjetly2003@gmail.com>
2024-04-01 17:46:16 -0400

    did stuff to admin album main

--- pa2/admin.java
+++ pa2/admin.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 /**
  * admin
  */
@@ -8,13 +10,21 @@ public class admin {
   private List<user> users=new ArrayList();
 
 
+  user get_user(String username){
+    for (user user : users){
+      if(username.equals(user.get_username())){
+        return user;
+      }
+    }
+    return null;
+  }
 
   void add_user(String username){
     users.add(new user(username));
   }
   
   void delete_user(String username){
-    for (user user : users){
+     for (user user : users){
       if(username.equals(user.get_username())){
         users.remove(user);
       }
