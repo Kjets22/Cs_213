e5a234007c442e7754906254063d42876d3b017c
Krish Jetly <Kjetly2003@gmail.com>
2024-04-01 16:14:56 -0400

    update

--- pa2/main.java
+++ pa2/main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * main
  */
 public class main {
   public static void main(String[] args){
     System.out.println("inside photo directory in the main the java file");
-
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+    String input = myObj.nextLine();
+    if (input.equals("admin")){
+      
+    }
+  }
+  string get_user_input(){
+    Scanner.my
   }
 }
+
