7d0f3b87debb8f33cdb5ef5427e3b4b9e8e791f3
Krish Jetly <Kjetly2003@gmail.com>
2024-04-05 12:29:24 -0400

    add function to album and photo class tried using lambdas should work

--- pa2/album.java
+++ pa2/album.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Calendar;
+import java.util.function.Consumer;
 /**
  * album
  */
 public class album {
+  private List<photo> photos = new ArrayList();
   String name;
-  
+  int photo_number=0;
+
   album(String name){
     this.name=name;
-  
   }
+
   String get_name(){
     return name;
   }
- 	
+
+  void add_photo(String location, Calendar date, List<tag> tags){
+    photos.add(new photo(location,date,tags,photo_number));
+    photo_number++;
+  }
+
+  void remove_photo(int photo_Id){
+    findphoto_action(photo_Id,photo->photos.remove(photo));
+    }
+
+  public void findphoto_action(int photo_Id,Consumer<photo> action){    
+    for (photo photo : photos){
+      if(photo_Id == photo.getPhoto_Id()){
+        action.accept(photo);      
+      }
+    }
+  }
+  void caption_photo(int photo_Id,String caption){
+    findphoto_action(photo_Id,photo->photo.change_capation(caption)); 
+  }  
 }
--- pa2/photo.java
+++ pa2/photo.java
@@ -2,14 +2,16 @@ import java.util.Calendar;
 import java.util.List;
 
 public class photo{
-
+    String caption; 
     String location;
     Calendar date;
     List<tag> tags;
-    public photo(String location, Calendar date, List<tag> tags){
+    int photo_Id;
+    public photo(String location, Calendar date, List<tag> tags,int photo_Id){
         this.location = location;
         this.date = date;
         this.tags = tags;
+        this.photo_Id=photo_Id;
     }
 
     public String getLocation(){
@@ -24,7 +26,13 @@ public class photo{
         return tags;
     }
 
-   
+    public int getPhoto_Id(){
+        return photo_Id;
+    }
+
+   public void change_capation(String caption){
+       this.caption=caption; 
+   }
 
 
 }
